bf0c169e3d8ed88415c6b975fbba1b305e070f63
Pavel Laparevich <laparevichpavel@gmail.com>
2022-07-16 15:33:45 +0300

    added icons and few styles

--- src/api/api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export const getData = () => {
-    debugger
-    return axios.get('https://raw.githubusercontent.com/alexnaidovich/frontend_test_task/master/dataset.json')
-}
